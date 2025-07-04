ea4e37d718640831f601b72b18f37bc5a7aa58d4
Zane-E <zhou-h@live.cn>
2025-07-04 13:40:28 +0800

    Update

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -3,6 +3,7 @@
 # General patches
 echo "Copying General patches..."
 cp -f ${GITHUB_WORKSPACE}/patch/config/* config/kernel/
+cp -f ${GITHUB_WORKSPACE}/patch/boards/* config/boards/
 cp -f ${GITHUB_WORKSPACE}/patch/sbin/* packages/bsp/common/usr/sbin/
 
 # T4 Patches
@@ -14,7 +15,6 @@ cp -f ${GITHUB_WORKSPACE}/patch/T4/t4.patch patch/kernel/archive/rockchip64-6.12
 
 # 5C Patches
 echo "Copying 5C patches..."
-cp -f ${GITHUB_WORKSPACE}/patch/5C/rock-5c.conf config/boards/
 cp -f ${GITHUB_WORKSPACE}/patch/5C/reopen_disabled_nodes.patch patch/u-boot/legacy/u-boot-radxa-rk35xx/board_rock-5c/
 cp -f ${GITHUB_WORKSPACE}/patch/5C/fix-CPU-information.patch patch/kernel/rk35xx-vendor-6.1/
 cp -f ${GITHUB_WORKSPACE}/patch/5C/diyfan.patch patch/kernel/rk35xx-vendor-6.1/
@@ -25,7 +25,6 @@ cp -f ${GITHUB_WORKSPACE}/patch/N1/fix-n1-1.patch patch/kernel/archive/meson64-6
 cp -f ${GITHUB_WORKSPACE}/patch/N1/fix-n1-2.patch patch/kernel/archive/meson64-6.12/
 cp -f ${GITHUB_WORKSPACE}/patch/N1/fix-n1-1.patch patch/kernel/archive/meson64-6.15/
 cp -f ${GITHUB_WORKSPACE}/patch/N1/fix-n1-2.patch patch/kernel/archive/meson64-6.15/
-cp -f ${GITHUB_WORKSPACE}/patch/N1/aml-s9xx-box.tvb config/boards/
 cp -f ${GITHUB_WORKSPACE}/patch/N1/u-boot.ext config/optional/boards/aml-s9xx-box/_packages/bsp-cli/boot/
 
 # X2 Patches
@@ -33,17 +32,12 @@ echo "Copying X2 patches..."
 cp -f ${GITHUB_WORKSPACE}/patch/X2/rk3566-panther-x2.dts patch/kernel/archive/rockchip64-6.12/dt/
 cp -f ${GITHUB_WORKSPACE}/patch/X2/rk3566-panther-x2.dts patch/kernel/archive/rockchip64-6.16/dt/
 cp -r ${GITHUB_WORKSPACE}/patch/X2/dt patch/kernel/rk35xx-vendor-6.1/
-cp -f ${GITHUB_WORKSPACE}/patch/X2/panther-x2.csc config/boards/
 
 # JP Patches
 echo "Copying JP patches..."
 cp -f ${GITHUB_WORKSPACE}/patch/JP/rk3566-jp-tvbox.dts patch/kernel/archive/rockchip64-6.12/dt/
 cp -f ${GITHUB_WORKSPACE}/patch/JP/rk3566-jp-tvbox.dts patch/kernel/archive/rockchip64-6.16/dt/
 cp -f ${GITHUB_WORKSPACE}/patch/JP/dt/rk3566-jp-tvbox.dts patch/kernel/rk35xx-vendor-6.1/dt/
-cp -f ${GITHUB_WORKSPACE}/patch/JP/jp-tvbox.conf config/boards/
-
-# station-m2
-cp -f ${GITHUB_WORKSPACE}/patch/M2/station-m2.csc config/boards/
 
 # Remove '-unofficial' from the VENDOR name
 sed -i 's|Armbian-unofficial|Armbian|g' lib/functions/configuration/main-config.sh
