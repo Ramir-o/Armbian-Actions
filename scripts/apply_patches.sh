12356faa5988316c65a8b7d131b49a442340366f
Ramir-o <zhouba1516@gmail.com>
2026-09-14 12:27:48 +0800

    Update apply_patches.sh

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -141,12 +141,11 @@ cp -f ${GITHUB_WORKSPACE}/patch/TVI/rk3399-tvi3315a.dts patch/kernel/archive/roc
 cp -f ${GITHUB_WORKSPACE}/patch/TVI/rk3399-tvi3315a.dts patch/kernel/archive/rockchip64-7.2/dt/
 
 # Radxa-cubie-a5e Patches for 1G ram
-cp -f ${GITHUB_WORKSPACE}/patch/A5E/edit-dram-parameters.patch patch/u-boot/v2026.01/board_radxa-cubie-a5e/
+cp -f ${GITHUB_WORKSPACE}/patch/A5E/sunxi-dram-a523-alternative-parameters.patch patch/u-boot/v2026.01/board_radxa-cubie-a5e/
 cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/sunxi-6.18/patches.armbian/
 cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/sunxi-7.2/patches.armbian/
 sed -i '475a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-6.18/series.conf
 sed -i '585a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-7.2/series.conf
-cp -f ${GITHUB_WORKSPACE}/patch/A5E/0026-clocksource-drivers-sun4i-Never-program-a-zero-inter.patch patch/kernel/archive/sunxi-7.2/patches.megous/fixes-7.2/
 
 # Uefi-arm64 Patches
 cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/uefi-arm64-6.18/
